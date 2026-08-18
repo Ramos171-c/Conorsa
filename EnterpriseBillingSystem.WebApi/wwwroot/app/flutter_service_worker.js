26a683de8e62c89c2c681c82755400dd4eb994d1
RAMOS1777 <corteztomas96@gmail.com>
2026-08-18 11:05:01 -0600

    build: update Flutter Web with hourly image cache-buster fix

--- EnterpriseBillingSystem.WebApi/wwwroot/app/flutter_service_worker.js
+++ EnterpriseBillingSystem.WebApi/wwwroot/app/flutter_service_worker.js
@@ -112,14 +112,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3caa9d7d290f980da729f0d07c49fea7
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6124b75898beb04d9419ccba388e1c46",
+"flutter_bootstrap.js": "56eaab2d119e8412459b7fe56a6f08f7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "6ef258c9cf13f6e260ac0aa61e9fb7c2",
 "/": "6ef258c9cf13f6e260ac0aa61e9fb7c2",
-"main.dart.js": "008d77d4ce62f9711994f83ef80d478f",
+"main.dart.js": "ca189ebc68bb236fbf03ee97ce80067a",
 "manifest.json": "f4ae899a4eeddbb28d2ba60be91be037",
 "version.json": "a7e55a991cc69f60ca0a7cbd4169ae40"};
 // The application shell files that are downloaded before a service worker can
